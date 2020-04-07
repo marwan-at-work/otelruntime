c752f4bdb1f797b3aa1bb21da167200c20b7f0e1
Marwan Sulaiman <marwan.sameer@gmail.com>
2020-04-07 01:09:47 -0400

    add options

--- otelruntime.go
+++ otelruntime.go
@@ -14,6 +14,7 @@ import (
 // below
 var ProcessKey = key.New("go_process")
 
+// tag keys
 const (
 	Goroutines = "cpu_goroutines"
 	Heap       = "heap_alloc"
@@ -21,12 +22,40 @@ const (
 	SysHeap    = "sys_heap"
 )
 
+// Config for the runtime reporter
+type Config struct {
+	meter metric.Meter
+	name  string
+}
+
+// Option allows changes to the Config
+type Option func(c *Config)
+
+// WithMeter sets the meter that the runtime reporter will use
+// otherwise global.Meter is used by default.
+func WithMeter(meter metric.Meter) Option {
+	return func(c *Config) {
+		c.meter = meter
+	}
+}
+
+// WithMetricName customizes the exported metric name or otherwise
+// will be called go_runtime by default.
+func WithMetricName(name string) Option {
+	return func(c *Config) {
+		c.name = name
+	}
+}
+
 // Register registers an infinite loop that
 // profiles the process and reports the views
-func Register() {
-	meter := global.Meter("otelruntime")
-	mm := metric.Must(meter)
-	mm.RegisterInt64Observer("go_process", func(result metric.Int64ObserverResult) {
+func Register(options ...Option) {
+	c := &Config{meter: global.Meter("otelruntime"), name: "go_runtime"}
+	for _, o := range options {
+		o(c)
+	}
+	mm := metric.Must(c.meter)
+	mm.RegisterInt64Observer(c.name, func(result metric.Int64ObserverResult) {
 		var ms runtime.MemStats
 		runtime.ReadMemStats(&ms)
 		result.Observe(int64(runtime.NumCPU()), ProcessKey.String(Goroutines))
